697c5d6e9a4b74012dd8ac2e1c09acb4c67c05ad
pajarmuharam14 <pajarmuharam321@gmail.com>
2023-10-16 17:25:10 +0700

    add svg in header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
 /* eslint-disable react/prop-types */
+
+import svg from "../assets/react.svg";
 const Header = ({ text }) => {
   return (
     <>
       <h1 className="text-center my-8 text-2xl text-red-700 font-semibold capitalize">{text}</h1>
+      <img src={svg} />
     </>
   );
 };
