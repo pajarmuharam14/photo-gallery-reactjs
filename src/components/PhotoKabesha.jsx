39281d8b4d247e2eeefd6bb2e8794e856ce28c15
pajarmuharam14 <pajarmuharam321@gmail.com>
2023-10-16 18:27:15 +0700

    move data members in json from js file

--- src/components/PhotoKabesha.jsx
+++ src/components/PhotoKabesha.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-key */
 import { Link } from "react-router-dom";
-import { dataMembers } from "../json/data/dataMembers.json";
+import dataMembers from "../datas/dataMember";
 
 const PhotoKabesha = () => {
   return (
