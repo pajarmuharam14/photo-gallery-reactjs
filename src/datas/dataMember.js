23bbb1b809a11a75c84b5cf25304a33c4cfee003
pajarmuharam14 <pajarmuharam321@gmail.com>
2023-10-17 11:31:00 +0700

    commit

--- src/datas/dataMember.js
+++ src/datas/dataMember.js
@@ -25,7 +25,7 @@ const dataMembers = [
     id: 4,
     name: "Anindya Ramadhani",
     nikcName: "Anindya",
-    kabesha: "/images/kabesha/kabesha-anindya.jpg",
+    kabesha: "/images/kabesha/kabesha-ashel.jpg",
     page: "/anindya",
   },
   {
