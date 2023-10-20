eb939d333a58233e5e72054807e3242316030571
pajarmuharam14 <pajarmuharam321@gmail.com>
2023-10-21 03:17:16 +0700

    add photo greesel and gracie

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,8 @@
 /* eslint-disable react/prop-types */
-import reactSvg from "../assets/react.svg";
-
 const Header = ({ text }) => {
   return (
     <>
       <h1 className="text-center my-8 text-2xl text-red-700 font-semibold capitalize">{text}</h1>
-      <img src={reactSvg} />
     </>
   );
 };
